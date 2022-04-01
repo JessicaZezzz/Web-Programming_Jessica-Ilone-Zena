005fba025bfea7880de00d23329792466c366345
Jessica Ilone <72442154+JessicaZezzz@users.noreply.github.com>
2022-04-01 16:15:36 +0700

    Delete config.php

--- SL1_2440092391_Jessica Ilone Zena/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server = "localhost";
-    $UserName = "root";
-    $PassworD = "";
-    $db_name = "sl-database";
-    $connection = mysqli_connect($server, $UserName, $PassworD, $db_name);
-
-?>
